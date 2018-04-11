5d303573a73dd2b3a2913ed5149704271e556ab1
Guillaume Florent <guillaume.florent@tutanota.com>
2018-04-11 13:05:09 +0200

    Path problem in test_cad_file_cache.py

--- tests/test_cad_file_cache.py
+++ tests/test_cad_file_cache.py
@@ -4,6 +4,7 @@
 r"""transformations.py tests"""
 
 from time import time
+from corelib.core.files import path_from_file
 from osvcad.nodes import PartGeometryNode
 
 
@@ -11,9 +12,11 @@ def test_cache():
     r"""Test the angle_between vectors of transformations.py"""
 
     t0 = time()
-    _ = PartGeometryNode.from_stepzip("cad_files/rim.stepzip")
+    _ = PartGeometryNode.from_stepzip(path_from_file(__file__,
+                                                     "./cad_files/rim.stepzip"))
     t1 = time()
-    _ = PartGeometryNode.from_stepzip("cad_files/rim.stepzip")
+    _ = PartGeometryNode.from_stepzip(path_from_file(__file__,
+                                                     "./cad_files/rim.stepzip"))
     t2 = time()
 
     assert 100 * (t2 - t1) < (t1 - t0)
